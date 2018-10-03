77cb1bff4ebb04b037a6358ddf0c644fd654bf34
Roee <roy.shap1@gmail.com>
2018-10-03 11:07:05 +0300

    Added project site.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+	constructor(
+		private iconRegistry: MatIconRegistry,
+		private sanitizer: DomSanitizer
+	) {
+		this.iconRegistry.addSvgIcon(
+			'linkedIn',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/linkedIn.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'resume',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/resume.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'code',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/code.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'code',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/code.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'new_tab',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/new_tab.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'github',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/github.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'bitbucket',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/bitbucket.svg')
+		);
+		this.iconRegistry.addSvgIcon(
+			'npm',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/npm.svg')
+		);
+	}
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material';
 
 
 @Component({
@@ -9,34 +7,6 @@ import { MatIconRegistry } from '@angular/material';
 	styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-	constructor(
-		private iconRegistry: MatIconRegistry,
-		private sanitizer: DomSanitizer
-	) {
-		this.iconRegistry.addSvgIcon(
-			'linkedIn',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/linkedIn.svg')
-		);
-		this.iconRegistry.addSvgIcon(
-			'resume',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/resume.svg')
-		);
-		this.iconRegistry.addSvgIcon(
-			'code',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/code.svg')
-		);
-		this.iconRegistry.addSvgIcon(
-			'github',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/github.svg')
-		);
-		this.iconRegistry.addSvgIcon(
-			'bitbucket',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/bitbucket.svg')
-		);
-		this.iconRegistry.addSvgIcon(
-			'npm',
-			this.sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/npm.svg')
-		);
-	}
+	constructor() {}
 	ngOnInit() {}
 }
