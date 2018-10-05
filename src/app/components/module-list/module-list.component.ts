ab67681e5d44fa463a2789f0bbe857cf13cb674a
Roee <roy.shap1@gmail.com>
2018-10-05 15:15:09 +0300

    Added npm package details.

--- src/app/components/module-list/module-list.component.ts
+++ src/app/components/module-list/module-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { map, tap, switchMap } from 'rxjs/operators';
 
 import { PortfolioService } from '../../services/portfolio.service';
 
@@ -36,7 +36,26 @@ export class ModuleListComponent implements OnInit {
 			.pipe(
 				map(res => (res)? res.results : []),
 				tap(resArr => this.modules = resArr),
-				tap(() => this.cdr.detectChanges()),
+				switchMap(resArr => {
+					const obsArr = resArr.map((res, index) =>
+						this.portfolio.getNpmPackage(res.package.name)
+							.pipe(tap(pkgDtls => {
+								pkgDtls.totalDownloads = this.countDownloads(pkgDtls)
+								this.modules[index].packageDetails = pkgDtls;
+							}))
+					);
+					return forkJoin(obsArr);
+				}),
+				tap(() => console.log(this.modules)),
+				tap(_ => this.cdr.detectChanges()),
 			);
+	}
+
+	private countDownloads(pkg: any): number {
+		return pkg
+			.collected
+			.npm
+			.downloads
+			.reduce((accu, curr) => accu += curr.count, 0);
 	}
 }
