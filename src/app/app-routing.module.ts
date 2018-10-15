fe38c2e4ead5f653698b4e190ea46c9e000da87a
Roee <roy.shap1@gmail.com>
2018-10-15 14:48:22 +0300

    Changed route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { ModuleListComponent } from './components/module-list/module-list.compon
 const routes: Routes = [
 	{ path: '', component: HomeComponent, pathMatch: 'full' },
 	{ path: 'projects', component: ProjectListComponent, pathMatch: 'full' },
-	{ path: 'npm', component: ModuleListComponent, pathMatch: 'full' },
+	{ path: 'modules', component: ModuleListComponent, pathMatch: 'full' },
 	{ path: '**', redirectTo: '' , pathMatch: 'full' },
 ];
 
